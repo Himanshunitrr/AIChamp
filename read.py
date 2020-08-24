10443252a48e5e5e0272666032954709a69b28d1
Himanshunitrr <himanshumauryacrz@gmail.com>
2020-08-24 21:34:14 +0530

    first commit

---
+++ read.py
@@ -0,0 +1,36 @@
+import io
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+from string import Template
+
+def extract_text_from_pdf(pdf_path):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle)
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+    with open(pdf_path, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+
+        text = fake_file_handle.getvalue()
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+
+    if text:
+        return text
+
+
+if __name__ == '__main__':
+  x = 1
+  while x <= 50:
+    # "Hello, %s. You are %s." % (name, age)
+    str = "D:/Documents/AIChamp/Profiles/Profile (%s).pdf" % (x)
+    x += 1
+    print(extract_text_from_pdf(str))
